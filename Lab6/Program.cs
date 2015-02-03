619a07dbdae47a3ba3145d9b8ea39e7751571d21
Lê Nhữ Trung <nhutrung2610@gmail.com>
2015-02-03 21:45:02 +0700

    Thay đổi lần 1

---
+++ Lab6/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace Lab6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SqlConnection myConnect = new SqlConnection("Server=trunglnpa00124-lab6.mssql.somee.com; user id=trunglnpa00124; password=Khanhvy0211;database=trunglnpa00124-lab6");
+            try
+            {
+                myConnect.Open();
+                Console.WriteLine(myConnect.State.ToString());
+                Console.WriteLine("Ket noi thanh cong!");
+                Console.WriteLine("Version 1");
+                Console.ReadLine();
+            }
+            catch (SqlException ex)
+            {
+                
+                Console.WriteLine(ex.Message);
+                Console.ReadLine();
+            }
+        }
+    }
+}
